7e2247a524cd9b692c01660303e2a6c70e388fe1
nishino-shuji <nishino-shuji@dmm.com>
2022-02-17 21:46:44 +0900

    init

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import { render } from "solid-js/web";
+import "tailwindcss/tailwind.css";
+import "../css/style.css";
+
+const App = () => {
+    return <div class="text-red-400">stest</div>
+};
+
+render(() => <App />, document.getElementById("root"))
